56b7355d1d137b9e197653a50f80fadd09818da7
Alex Hartford <alex@oneblank.io>
2020-08-04 13:45:46 -0700

    [Android] Stop scanning after first

--- android/src/main/kotlin/net/touchcapture/qr/flutterqr/QRView.kt
+++ android/src/main/kotlin/net/touchcapture/qr/flutterqr/QRView.kt
@@ -149,7 +149,7 @@ class QRView(private val registrar: PluginRegistry.Registrar, id: Int) :
                 object : BarcodeCallback {
                     override fun barcodeResult(result: BarcodeResult) {
                         channel.invokeMethod("onRecognizeQR", result.text)
-                        barcodeView.stopDecoding()
+                        barcodeView?.stopDecoding()
                     }
 
                     override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
